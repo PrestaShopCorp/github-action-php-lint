0cbd2f6ce0383bf2ad44e8d841e6cd1f7894ea55
Thomas Nabord <thomas.nabord@prestashop.com>
2019-04-03 16:26:26 +0200

    Find all PHP files and display output in case of error

--- 5.6/entrypoint.sh
+++ 5.6/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-sh -c "! (find . $* -print0 | xargs -0 -n1 -P4 php -l | grep -q \"Parse error\")"
+sh -c "! (find . -name \"*.php\" $* -print0 | xargs -0 -n1 -P4 php -l | grep \"Parse error\")"
